db9c24ec53e0ce33e849204f985b003a8f1d77e8
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-04 21:00:42 +0300

    task columnda cardların render edilmesi

--- src/components/TaskColumn.tsx
+++ src/components/TaskColumn.tsx
@@ -1,18 +1,36 @@
+import { Col } from "react-bootstrap";
 import { Task } from "../types/Task";
+import { Droppable } from "@hello-pangea/dnd";
+import TaskCard from "./TaskCard";
 
 interface TaskColumnProps {
     status: string;
-    task: Task;
+    tasks: Task[];
 }
 
 const TaskColumn = ({ status, tasks }: TaskColumnProps) => {
 
     return (
-        <div>
-            taskcolumn
-        </div>
+        <Col>
+            <h3 className="text-center">{status}</h3>
+            <Droppable droppableId={status} >
+                {(provided, snapshot) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps} style={{
+                        minHeight: "500px",
+                        padding: "10px",
+                        background: snapshot.isDraggingOver ? "#e9ecef" : "#c6c9cc"
+                    }} >
+                        {
+                            tasks.map((task, index) => (
+                                <TaskCard key={task.id} task={task} index={index} />
+                            ))
+                        }
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </Col>
     )
-
 }
 
 export default TaskColumn;
