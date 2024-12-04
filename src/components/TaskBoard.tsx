c92a66f186c5f1ce9be78b5ccb5d8a2c0608c841
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-04 22:16:24 +0300

    array indeksine göre statuslerde geçiş yapabilme

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -14,6 +14,48 @@ const TaskBoard = ({ tasks, setTasks }: TaskBoardProps) => {
 
     const statuses: Task["status"][] = ["Todo", "In Progress", "Done"];
     const onDragEnd = (result: DropResult) => {
+        const { destination, source, draggableId } = result;
+        if (!destination) return;
+
+        if (destination.droppableId === source.droppableId && destination.index === source.index) {
+            return;
+        }
+
+        const draggedTask = tasks.find((task) => task.id === draggableId);
+
+        const updatedTask: Task = {
+            ...draggedTask,
+            status: destination.droppableId as Task["status"]
+        }
+
+        const newTasks = tasks.filter((task) => task.id !== draggableId)
+
+
+        const destinationTasks = newTasks.filter((task) => task.status === destination.droppableId)
+
+        // 0.indeksi temsil eder yani sürükledigimiz taskin arrayin konumunu temsil eder
+        let insertAt = 0;
+        if (destination.index === 0) {
+            // Hedef konumda bulunan aynı status degerine sahip ilk elemanı buluyor
+            const firstTaskInDestination = newTasks.find((task) => task.status === destination.droppableId)
+            if (firstTaskInDestination) {
+                // Bulunan elemanın indeksini bulma
+                insertAt = newTasks.indexOf(firstTaskInDestination);
+            } else {
+                insertAt = newTasks.length;
+            }
+        } else {
+            const prevTaskInDestination = destinationTasks[destination.index];
+            if (prevTaskInDestination) {
+                insertAt = newTasks.indexOf(prevTaskInDestination) + 1;
+
+            } else {
+                insertAt = newTasks.length;
+            }
+        }
+
+        newTasks.splice(insertAt, 0, updatedTask)
+        setTasks(newTasks)
 
 
     };
